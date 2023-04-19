eae2ab415e7a7f5da3c1b5991ae43c98ad9c2322
Fabio Sirigu <fabio.sirigu.1995@gmail.com>
2023-04-19 09:15:17 +0200

    styled input

--- src/components/Input/styled.ts
+++ src/components/Input/styled.ts
@@ -1,18 +1,19 @@
 // import { Icon } from '@fortawesome/fontawesome-svg-core'
 
 import styled from 'styled-components'
-import { ThemeColor } from '../../style/theme'
+import { ThemeColor, ThemeWidth } from '../../style/theme'
 
 export type InputProps = {
   background?: ThemeColor
   colorText?: ThemeColor
+  inputWidth?: ThemeWidth
   placeholder?: string
   radius?: number
   border?: string
 }
 
 export const StyledInput = styled.input<InputProps>`
-  width: 150px;
+  width: ${({ theme, inputWidth }) => theme.sizes[inputWidth || 'lg'] * 4}px;
   background-color: transparent;
   color: ${({ theme, colorText }) => theme.colors[colorText || 'text']};
   font-style: italic;
