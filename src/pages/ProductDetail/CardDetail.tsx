4d77e0131a18fffd6b0f60d26f78cd7e12444d1d
Fabio Sirigu <fabio.sirigu.1995@gmail.com>
2023-04-27 11:44:45 +0200

    add component card detail

---
+++ src/pages/ProductDetail/CardDetail.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getProductById } from '../../components/api'
+import { ProductDto } from '../../components/api/types'
+import { Counter } from '../../components/Counter'
+import { Image } from '../../components/Image'
+import { Price } from '../../components/Price'
+import { Text } from '../../components/Text'
+import {
+  StyledButton,
+  DetailCard,
+  ImageWrapper,
+  PriceWrapper,
+  TextWrapper
+} from './styled'
+
+export const CardDetail = () => {
+  const params = useParams()
+  const id = params.id!
+  console.log(id)
+
+  const [singleProducts, setSingleProducts] = useState<ProductDto>()
+
+  useEffect(() => {
+    getProductById(id).then((data) => setSingleProducts(data))
+  }, [])
+  return (
+    <DetailCard>
+      <TextWrapper>
+        <Text color="text" variant="h1">
+          {singleProducts?.name}
+        </Text>
+        <Text color="text" variant="h6">
+          Rating
+        </Text>
+        <Text color="textLight" variant="p">
+          {singleProducts?.description}
+        </Text>
+        <Text variant="h5" color="textLight">
+          Ingredients
+        </Text>
+        <PriceWrapper>
+          <Price title={singleProducts?.price.value} />
+          <Counter />
+        </PriceWrapper>
+        <StyledButton color="secondary" title="Order Now" font="h4" radius={3} />
+      </TextWrapper>
+      <ImageWrapper>
+        <Image url={singleProducts?.imageUrl} />
+      </ImageWrapper>
+    </DetailCard>
+  )
+}
--- src/pages/ProductDetail/styled.ts
+++ src/pages/ProductDetail/styled.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
+import { Button } from '../../components/Button'
 
 export const StyledDetails = styled.div`
   padding: 2rem;
+  position: relative;
 `
 
 export const RowButton = styled.div`
@@ -28,3 +30,18 @@ export const PriceWrapper = styled.div`
   display: flex;
   gap: 0.8rem;
 `
+
+export const StyledButton = styled(Button)`
+  width: 200px;
+  padding: 1rem;
+`
+
+/* export const BackgroundPattern = styled.div`
+  position: absolute;
+  width: 40%;
+  background: rgba(0, 0, 0, 0.5);
+  height: 100%;
+  right: 0;
+  top: 0;
+  z-index: -1;
+` */
