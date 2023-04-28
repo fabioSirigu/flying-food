00253ea597cfe98003d247150edf3753c9f74d16
Fabio Sirigu <fabio.sirigu.1995@gmail.com>
2023-04-28 11:41:18 +0200

    edit not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,3 +1,24 @@
+import { useNavigate } from 'react-router-dom'
+import { DefaultButton } from '../../components/Button/DefaultButton'
+import { Text } from '../../components/Text'
+import { StyledText } from './styled'
+
 export const NotFound = () => {
-  return <div>Error, not found!</div>
+  const navigate = useNavigate()
+  return (
+    <div className="App notFound">
+      <StyledText>
+        <Text variant="h1" color="primary">
+          Error, not found!
+        </Text>
+        <DefaultButton
+          padding="md"
+          color="secondary"
+          colorText="text"
+          title="Back Catalog"
+          onClick={() => navigate('/catalog')}
+        />
+      </StyledText>
+    </div>
+  )
 }
---
+++ src/pages/NotFound/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledText = styled.div`
+  width: 80%;
+  height: 100vh;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`
