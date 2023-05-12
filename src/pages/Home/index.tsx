67d49183f77e63383e1ee58f4bf75f362509d38f
Fabio Sirigu <fabio.sirigu.1995@gmail.com>
2023-05-12 14:17:31 +0200

    feat: minor change layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,21 @@
-import { memo } from 'react'
-import { Image } from '../../components/Image'
+import { memo, useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Text } from '../../components/Text'
-import { StyledMain } from './styled'
+import { StyledBoxTitle, StyledMain } from './styled'
 
 export const Home = memo(() => {
+  const navigate = useNavigate()
+
+  const handlenavigate = useCallback(() => navigate('/catalog'), [navigate])
+
   return (
     <div className="App">
       <StyledMain>
-        <Text weight="bold" variant="h1" color="text">
-          Welcome To FlyingFood
-        </Text>
+        <StyledBoxTitle onClick={handlenavigate}>
+          <Text weight="bold" variant="h1" color="text">
+            Welcome To FlyingFood
+          </Text>
+        </StyledBoxTitle>
       </StyledMain>
     </div>
   )
--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -12,13 +12,26 @@ const gradient = keyframes`
     }
 `
 
+const scale = keyframes`
+  0% { scale: 1; cursor:pointer; letter-spacing:0.5rem;}
+  50% { scale: 1.2; cursor:pointer; letter-spacing:0.75rem;}
+  100% { scale: 1;cursor:pointer; letter-spacing:0.5rem;}`
+
 export const StyledMain = styled.div`
-  background: linear-gradient(300deg, #ffffff, #039292, #000000);
-  background-size: 180% 180%;
-  animation: ${gradient} 10s ease infinite;
+  background: linear-gradient(300deg, #000000, #039292, #ffffff, #039292, #000000);
+  background-size: 200% 200%;
+  animation: ${gradient} 15s ease infinite;
   border-radius: 25px;
   height: calc(100vh - 150px);
   display: flex;
   justify-content: center;
   align-items: center;
+`
+
+export const StyledBoxTitle = styled.div`
+  background: rgba(0, 0, 0, 0.1);
+  padding: 1rem 2rem;
+  border: 1px solid white;
+  border-radius: 20px;
+  animation: ${scale} 5s infinite ease-in;
 `
