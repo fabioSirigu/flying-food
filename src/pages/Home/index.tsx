0eae47e4fb734e6e3555bd9ad74b227162a91853
Fabio Sirigu <fabio.sirigu.1995@gmail.com>
2023-05-12 12:12:32 +0200

    feat: change layout homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,16 @@
 import { memo } from 'react'
+import { Image } from '../../components/Image'
 import { Text } from '../../components/Text'
+import { StyledMain } from './styled'
 
 export const Home = memo(() => {
   return (
     <div className="App">
-      <Text variant="h1" color="text">
-        Welcome To Flying Food
-      </Text>
+      <StyledMain>
+        <Text weight="bold" variant="h1" color="text">
+          Welcome To FlyingFood
+        </Text>
+      </StyledMain>
     </div>
   )
 })
--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -1,42 +1,24 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
-export const RowWrapper = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  gap: 0.5rem;
-  margin-top: 1rem;
-`
-export const SliderWrapper = styled.div`
-  height: 150px;
-  width: 300px;
-  background: lightgreen;
-`
-
-export const CategoryWrapper = styled.div`
-  display: flex;
-  gap: 1rem;
-`
-export const TextWrapper = styled.div``
-
-export const SelectIconWrapper = styled.div`
-  display: flex;
-  gap: 0.8rem;
-  padding: 0 1rem;
-`
-export const SelectWrapper = styled.div`
-  display: flex;
-  gap: 0.8rem;
+const gradient = keyframes`
+  0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
 `
 
-export const IconWrapper = styled.div`
+export const StyledMain = styled.div`
+  background: linear-gradient(300deg, #ffffff, #039292, #000000);
+  background-size: 180% 180%;
+  animation: ${gradient} 10s ease infinite;
+  border-radius: 25px;
+  height: calc(100vh - 150px);
   display: flex;
-  gap: 0.4rem;
-`
-export const ProductWrapper = styled.div`
-  margin-top: 2rem;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  row-gap: 50px;
-  column-gap: 32px;
+  justify-content: center;
+  align-items: center;
 `
